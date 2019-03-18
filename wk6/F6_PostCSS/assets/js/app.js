04bc091de73bf895d9cfe35ef4ed7d8d045b2aec
chsyu <chsyu@tea.ntue.edu.tw>
2019-03-18 22:42:46 +0800

    wk6: PostCSS is ready

---
+++ wk6/F6_PostCSS/assets/js/app.js
@@ -0,0 +1,15 @@
+import $ from 'jquery';
+import 'bootstrap';
+import chsyu from '../images/chsyu.jpg';
+import app from '../images/app.jpg';
+import f2e from '../images/f2e.jpg';
+import iot from '../images/iot.jpg';
+import js from '../images/js.jpg';
+import '../css/bootstrap.min.css';
+import '../css/style.css';
+
+$('#image__chsyu').attr("src", chsyu);
+$('#image__f2e').attr("src", f2e);
+$('#image__iot').attr("src", iot);
+$('#image__app').attr("src", app);
+$('#image__js').attr("src", js);
